3eae4cc0ea7565dee2629ded7e3486b1a2d3cb9e
Gehana Gaikwad C <102535006+gehanagaikwadc@users.noreply.github.com>
2024-07-11 19:30:24 +0530

    Update Program_2.c

--- Program_2.c
+++ Program_2.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void printSize(char dataType[]) {
+    if (strcmp(dataType, "Integer") == 0) {
+        printf("4\n");
+    } else if (strcmp(dataType, "Long") == 0) {
+        printf("8\n");
+    } else if (strcmp(dataType, "Float") == 0) {
+        printf("4\n");
+    } else if (strcmp(dataType, "Double") == 0) {
+        printf("8\n");
+    } else if (strcmp(dataType, "Character") == 0) {
+        printf("1\n");
+    } else {
+        printf("Unknown data type\n");
+    }
+}
+
+int main() {
+    char dataType[20];
+    printf("Enter data type: ");
+    scanf("%s", dataType);
+    
+    printSize(dataType);
+
+    return 0;
+}
 
