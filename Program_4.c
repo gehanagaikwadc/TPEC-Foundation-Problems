ecb63c6d319e7815df40c5a1637da62032219f33
Gehana Gaikwad C <102535006+gehanagaikwadc@users.noreply.github.com>
2024-07-22 19:46:01 +0530

    Update Program_4.c

--- Program_4.c
+++ Program_4.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    } else {
+        int a = 1, b = 1, c;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the position of the Fibonacci number: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Invalid input. Please enter a positive integer.\n");
+    } else {
+        int result = fibonacci(n);
+        printf("The %d-th Fibonacci number is %d\n", n, result);
+    }
+
+    return 0;
+}
 
