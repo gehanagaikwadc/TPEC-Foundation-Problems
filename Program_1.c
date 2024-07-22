c9383c6b005d98f0d554894b5ae22c99c677980c
Gehana Gaikwad C <102535006+gehanagaikwadc@users.noreply.github.com>
2024-07-22 19:44:11 +0530

    Update Program_1.c

--- Program_1.c
+++ Program_1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int check_character(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return 1;
+    } else if (c >= 'a' && c <= 'z') {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    char input_char;
+    printf("Enter a character: ");
+    scanf("%c", &input_char);
+    
+    int result = check_character(input_char);
+    printf("%d\n", result);
+    
+    return 0;
+}
 
